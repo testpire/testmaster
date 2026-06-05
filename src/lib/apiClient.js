4af2665814f92c4ce8a355948ba03bb1f18b1edd
araj <araj@taranawireless.com>
2026-06-06 01:33:11 +0530

    lead generation and rbac code

--- src/lib/apiClient.js
+++ src/lib/apiClient.js
@@ -2,9 +2,9 @@ import axios from 'axios';
 
 // Base API configuration
 //const API_BASE_URL = 'http://localhost:8080/api';
-const API_BASE_URL = 'https://testpire.v43d8nfv0vckm.ap-south-1.cs.amazonlightsail.com/api';
+//const API_BASE_URL = 'https://testpire.v43d8nfv0vckm.ap-south-1.cs.amazonlightsail.com/api';
 //const API_BASE_URL = 'http://80.225.250.247:8080/api';
-//const API_BASE_URL = 'https://ware-dictionaries-avenue-bishop.trycloudflare.com/api';
+const API_BASE_URL = 'https://hazards-muslim-breakdown-ccd.trycloudflare.com/api';
 
 // Create axios instance
 const apiClient = axios.create({
